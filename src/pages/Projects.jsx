63aec15f28010d5e9f0400f04e92d9fb195c616f
sujithv15 <64935098+sujithv15@users.noreply.github.com>
2023-08-05 15:16:37 -0400

    added projects message

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,6 +5,9 @@ const Projects = () => {
 	return (
 		<div className="page">
 			<div className="title">Projects</div>
+			<div className="text-center mx-52 my-8">
+				Projects are working, but are being updated regularly with fixes and new features. Feel free to take a tour of the apps and check them out so far!
+			</div>
 			<div className="flex flex-wrap justify-around bg-gray-100 rounded-lg ">
 				{
 					projects.map((project, index) => {
