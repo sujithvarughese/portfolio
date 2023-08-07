9ea0e27da99b0eec154a287d15055bdccd38e633
sujithv15 <64935098+sujithv15@users.noreply.github.com>
2023-08-07 03:26:18 -0400

    message update

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,11 +6,11 @@ const Projects = () => {
 		<div className="page">
 			<div className="title">Projects</div>
 			<div className="text-center mx-52 my-8">
-				Welcome to my projects page! These are a few projects I have been working on. They are being updated regularly with fixes and new features. Feel free to take a tour!
-				(Note: Some browsers are not retrieving data from server if cookies or cross-site tracking are disabled   )
+				Welcome to my projects page! These are a few projects I have been working on. I am still developing them, so they are being updated regularly with fixes and new features. I set up links to access the apps without creating an account, so feel free to take a tour! There are also GitHub links with source code that can be viewed!
+
 			</div>
 			<div className="text-center mx-52 my-8">
-				Update: Some browsers are causing CORS issues. Working on update...
+				Note: Some browsers are not retrieving data from server if cookies or cross-site tracking are disabled. Also, I am using free servers on netlify so might be a little slow to load at first.
 			</div>
 			<div className="flex flex-wrap justify-around bg-gray-100 rounded-lg ">
 				{
