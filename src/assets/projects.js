5e60e487ff4d515bdd6befe0b845200a25584f4d
sujithv15 <64935098+sujithv15@users.noreply.github.com>
2023-08-07 03:35:50 -0400

    message updates

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -8,7 +8,7 @@ export const projects = [
 		title: "bookshelf.",
 		cover: bookshelf,
 		caption: "Discover new reading. Organize your collection.",
-		details: "Bookshelf is a personal reading organizer that uses the OpenLibrary API to connect users to millions of books. Find books based on subject matter, take notes, and organize your favorite books into your own bookshelf",
+		details: "Bookshelf is a personal reading organizer that uses the OpenLibrary API to connect users to millions of books. Find books based on subject matter, take notes, and organize your favorite books into your own bookshelf!",
 		link: 'https://bookshelf-suj.netlify.app',
 		github: 'https://github.com/sujithvarughese/bookshelf-app'
 	},
@@ -24,7 +24,7 @@ export const projects = [
 		title: "Sketch",
 		cover: sketch,
 		caption: "The original Sketch App. Now in Color.",
-		details: "Etch-a-Sketch like never before. You pick the size. You pick the colors. The possibilities are endless",
+		details: "Etch-a-Sketch like never before. You pick the size. You pick the colors. The possibilities are endless.",
 		link: 'https://sketch-suj.netlify.app',
 		github: 'https://github.com/sujithvarughese/odin-etch-a-sketch/tree/main/react'
 	},
