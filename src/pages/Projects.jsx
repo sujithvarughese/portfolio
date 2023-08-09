d8c29cb8dbf3a879d422ba2b0e03d65cc2aeafc3
sujithv15 <64935098+sujithv15@users.noreply.github.com>
2023-08-09 02:22:15 -0400

    styling

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,11 +5,11 @@ const Projects = () => {
 	return (
 		<div className="page">
 			<div className="title">Projects</div>
-			<div className="text-center mx-52 md:mx-4 my-8">
+			<div className="text-center md:mx-52 my-8">
 				Welcome to my projects page! These are a few projects I have been working on. I am still developing them, so they are being updated regularly with fixes and new features. I set up links to access the apps without creating an account, so feel free to take a tour! There are also GitHub links with source code that can be viewed!
 
 			</div>
-			<div className="text-center mx-52 my-8">
+			<div className="text-center md:mx-52 my-8">
 				Note: Projects are optimized for desktop. Some browsers are not retrieving data from server if cookies or cross-site tracking are disabled. Also, I am using free servers on netlify so might be a little slow to load at first.
 			</div>
 			<div className="flex flex-wrap justify-around bg-gray-100 rounded-lg ">
