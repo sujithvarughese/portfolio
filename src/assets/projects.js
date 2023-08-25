6e83b73db17df06462e7431778a080e28b103123
sujithv15 <64935098+sujithv15@users.noreply.github.com>
2023-08-24 21:30:49 -0400

    changed order/styling

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -5,20 +5,20 @@ import blackjack from "./images/blackjack-cover.jpeg"
 
 export const projects = [
 	{
-		title: "bookshelf.",
-		cover: bookshelf,
-		caption: "Discover new reading. Organize your collection.",
-		details: "Bookshelf is a personal reading organizer that uses the OpenLibrary API to connect users to millions of books. Find books based on subject matter, take notes, and organize your favorite books into your own bookshelf!",
-		link: 'https://bookshelf-suj.netlify.app',
-		github: 'https://github.com/sujithvarughese/bookshelf-app'
-	},
-	{
 		title: "Property Management Assistant",
 		cover: property,
 		caption: "Manage all your properties with one simple App.",
 		details: "Whether you're managing a single unit or if you're a real estate mogul, the responsibilities of day-to-day operations can be overwhelming. Property Management Assistant simplifies your job by organizing your tasks and keeping track of all your units, all in one place.",
 		link: 'https://prop-management-assistant.netlify.app',
 		github: 'https://github.com/sujithvarughese/property-management-app'
+	},
+	{
+		title: "bookshelf.",
+		cover: bookshelf,
+		caption: "Discover new reading. Organize your collection.",
+		details: "Bookshelf is a personal reading organizer that uses the OpenLibrary API to connect users to millions of books. Find books based on subject matter, take notes, and organize your favorite books into your own bookshelf!",
+		link: 'https://bookshelf-suj.netlify.app',
+		github: 'https://github.com/sujithvarughese/bookshelf-app'
 	},
 	{
 		title: "Sketch",
