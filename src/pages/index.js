d9b7d84fa7ed6096a1f49ee88f97465acb066583
Sujith Varughese <sujithv15@gmail.com>
2023-12-19 01:39:14 -0500

    project previews update

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import AboutMe from "./AboutMe.jsx";
-import Contact from "./Contact.jsx";
-import Layout from './Layout.jsx'
-import Resume from "./Resume.jsx";
-import Portfolio from "./Portfolio.jsx";
-
-export { AboutMe, Contact, Layout, Resume, Portfolio }
--- src/ui/index.js
+++
@@ -1,9 +0,0 @@
-import ButtonPlain from "./ButtonPlain.jsx";
-import ButtonIcon from "./ButtonIcon.jsx";
-import Button from "./Button.jsx"
-import NavModal from "./NavModal.jsx";
-import LinkIcon from "./LinkIcon.jsx";
-import Input from "./Input.jsx"
-import Form from "./Form.jsx"
-import Textarea from "./Textarea.jsx";
-export { Button, ButtonIcon, ButtonPlain, NavModal, LinkIcon, Input, Form, Textarea }
