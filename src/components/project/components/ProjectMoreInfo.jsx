64a253537e033fbf768ba511a502b66e995d6793
Sujith Varughese <sujithv15@gmail.com>
2023-12-19 03:15:09 -0500

    spacing fixes- screen was overflowing on mobile version

--- src/components/project/components/ProjectMoreInfo.jsx
+++ src/components/project/components/ProjectMoreInfo.jsx
@@ -2,10 +2,14 @@ import classes from "../styles/Project.module.css";
 import {FaAnglesDown, FaAnglesUp} from "react-icons/fa6";
 import ButtonIcon from "../../../ui/ButtonIcon.jsx";
 import {ButtonPlain} from "../../../ui/index.js";
+import { motion } from "framer-motion"
 
 const ProjectMoreInfo = ({ showPreviews, setShowPreviews }) => {
     return (
-        <div className={classes.arrow}>
+        <motion.div
+            className={classes.arrow}
+            whileHover={{ scale: 1.1 }}
+        >
             <ButtonIcon className={classes.togglePreview} onClick={()=>setShowPreviews(!showPreviews)}
                         animate={{ rotate: showPreviews ? 180 : 0 }}
                         transition={{ duration: 0.01 }}
@@ -19,7 +23,7 @@ const ProjectMoreInfo = ({ showPreviews, setShowPreviews }) => {
             >
                 {showPreviews ? "Hide" : "More"}
             </ButtonPlain>
-        </div>
+        </motion.div>
 
 
 
