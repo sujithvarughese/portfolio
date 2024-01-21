f7e6329f68dcdf598d5b005f7fa31b53dc7c8312
Sujith Varughese <sujithv15@gmail.com>
2024-01-21 02:08:51 -0500

    screenshot styling update

--- client/src/ui/PhoneImageContainer.jsx
+++ client/src/ui/PhoneImageContainer.jsx
@@ -5,36 +5,33 @@ import fitnessSearchIMG from "../assets/images/simpler-fitness/search.png"
 import fitnessInstructionsIMG from "../assets/images/simpler-fitness/instructions.png"
 const PhoneImageContainer = () => {
   return (
-
-
-      <Box
+      <Container
         sx={{ perspective: "220em"}}
         position="relative"
-
       >
         <Image
           src={phone}
           position="absolute"
           top="0px"
-          left="35px"
-          width="285px"
-          height="742px"
+          left="25px"
+          width="305px"
+          height="740px"
         >
 
         </Image>
         <Image
           src={fitnessBrowseIMG}
           alt="fit1"
-          transform="rotateY(326deg) skewY(-3.5deg)"
+          transform="rotateY(327deg) skewY(-3.5deg)"
           borderRadius="50px"
           position="absolute"
           top="20px"
           left="0"
-          width="328px"
+          width="327px"
           overflow="hidden"
 
         />
-      </Box>
+      </Container>
 
   )
 }
