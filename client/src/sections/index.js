65a87d5fe512289dc10a1f6ce2af1402dfa94f76
Sujith Varughese <sujithv15@gmail.com>
2024-01-25 16:59:55 -0500

    bio updates

--- client/src/sections/index.js
+++
@@ -1,6 +0,0 @@
-import ContactMe from './ContactMe.jsx'
-import Landing from './Landing.jsx'
-import Projects from './Projects.jsx'
-import Resume from './Resume.jsx'
-
-export { ContactMe, Landing, Projects, Resume }
